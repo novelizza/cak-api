b07e087f2789a6dcd6fdf5129090bd88811ce359
novelizza <novel.izza.fardana@gmail.com>
2021-06-08 21:52:14 +0700

    test crud create data 1c

--- index.php
+++ index.php
@@ -1,12 +1,5 @@
 <html>
 <body>
 <h1><center>What are you looking for ?</center></h1>
-<center>
-<?php
-include_once 'conn.php';
-?>
-<br>
-<!-- <a href="controller.php">Test</a> -->
-</center>
 </body>
 </html>
---
+++ pemesanan.php
@@ -0,0 +1,40 @@
+<?php
+include_once 'conn.php';
+
+$email = $_POST['email'];
+$judul = $_POST['judul'];
+$jenis = $_POST['jenis'];
+$jadwal = $_POST['jadwal'];
+$dewasa = $_POST['dewasa'];
+$anak = $_POST['anak'];
+$dateNow = date("Y-m-d h:i");
+
+$q1 = "SELECT id_film, hargaDewasa, hargaAnak FROM tb_film where judul='$judul'";
+$resultFilm = pg_query($dbconn,$q1);
+while ($row1 = pg_fetch_assoc($resultFilm)){
+    $id_film = $row1['id_film'];
+    $hargaDewasa = $row1['hargaDewasa'];
+    $hargaAnak = $row1['hargaAnak'];
+}
+
+$hargaTotal = ($dewasa*$hargaDewasa)+($anak*$hargaAnak);
+
+$q2 = "SELECT id_akun FROM tb_akun where email='$email'";
+$resultAkun = pg_query($dbconn,$q2);
+while ($row2 = pg_fetch_assoc($resultAkun)){
+    $id_akun = $row2['id_akun'];
+}
+
+$q = "INSERT INTO tb_pembelian (id_pembelian, id_akun, id_film, jenis, taggal, jumlahDewasa, jumlahAnak, hargaTotal, created_at, updated_at) VALUES (DEFAULT, '$id_akun', '$id_film', '$jenis', '$jadwal', '$dewasa', '$anak', '$hargaTotal', 'dateNow', 'dateNow')";
+$result = pg_query($dbconn,$q);
+
+if($result){
+    echo json_encode(array(
+        'status' => 'ok',
+    ));
+} else {
+    echo json_encode(array(
+        'status' => 'error'
+    ));
+}
+?>
