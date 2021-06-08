105afbe9ed97cb1c0c61addf9d0e816c0269f1d3
novelizza <novel.izza.fardana@gmail.com>
2021-06-08 20:37:13 +0700

    test crud create data 1c

--- login.php
+++ login.php
@@ -4,7 +4,7 @@ include_once 'conn.php';
 $email = $_GET['email'];
 $password = $_GET['password'];
 
-$result = pg_query($dbconn,"SELECT email, password FROM tb_akun where email='$email' AND password='$password'");
+$result = pg_query($dbconn,"SELECT email, password, nama FROM tb_akun where email='$email' AND password='$password'");
 if($result){
     while ($row = pg_fetch_assoc($result)){
         $emailNow = $row['email'];
