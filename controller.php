3586a4cd01a8077d9322450dde37d9e11d68c0fa
novelizza <novel.izza.fardana@gmail.com>
2021-06-08 15:54:44 +0700

    test crud create data 1b

--- controller.php
+++ controller.php
@@ -5,9 +5,8 @@ $email = $_GET['email'];
 $password = $_GET['password'];
 $nama = $_GET['nama'];
 $dateNow = date("Y-m-d h:i");
-$dateNowA = date("Y-m-d");
 
-$result = pg_query($dbconn,"INSERT INTO tb_akun (id_akun, email, password, phone_number, nama, created_at, updated_at) VALUES (DEFAULT, '$email', '$password', '08123456789', '$nama', '$dateNowA', '$dateNow')");
+$result = pg_query($dbconn,"INSERT INTO tb_akun (id_akun, email, password, phone_number, nama, created_at, updated_at) VALUES (DEFAULT, '$email', '$password', '08123456789', '$nama', '$dateNow', '$dateNow')");
 if($result){
     echo json_encode(array(
         'status' => 'ok'
