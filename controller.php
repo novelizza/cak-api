fa7280563784d38f1d3bfb81e886ab8d1a59b541
novelizza <novel.izza.fardana@gmail.com>
2021-06-08 14:39:04 +0700

    test crud 3

--- controller.php
+++ controller.php
@@ -1,18 +1,10 @@
 <?php
-class SiteController extends Controller
-{   
-    function actionSimpan_data()
-    {
-        // $model=new Data_siswaTbl();
-        // $model->id_mahasiswa=rand(100000,9999999);
-        // $model->nama_mahasiswa=$_POST['nama'];
-        // $model->kelas=$_POST['kelas'];
-        // $model->jenis_kelamin=$_POST['jenis_kelamin'];
-        // $model->no_handphone=$_POST['no_handphone'];
-        // $model->save();
-        echo json_encode(array(
-            'status'=>'oke'
-        ));
-    }
+include_once 'conn.php';
+
+$result = pg_query($dbconn, "SELECT email, password FROM tb_akun");
+if($result){
+    echo json_encode(array(
+        'status' => 'ok'
+    ));
 }
 ?>
