6ecde7e4d89ed2ff7fad05c91814a47b5a6135d9
novelizza <novel.izza.fardana@gmail.com>
2021-06-08 20:18:30 +0700

    test crud create data 1c

--- login.php
+++ login.php
@@ -6,7 +6,7 @@ $password = $_GET['password'];
 
 $result = pg_query($dbconn,"SELECT email, password FROM tb_akun where email='$email' AND password='$password'");
 if($result){
-    $nama = $result['nama'];
+    $nama = $result["nama"];
     echo json_encode(array(
         'status' => 'ok',
         'email' => $email,
